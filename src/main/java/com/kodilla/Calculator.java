08f7fce5096dd26f0e6b9867df21b2c1cd1e0421
dzejsiejcz <joel.chawraj@gmail.com>
2021-10-31 18:55:22 +0100

    Dodawanie i odejmowanie

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,22 @@
 package com.kodilla;
 
 public class Calculator {
+
+    public int addition(int a, int b) {
+        int subtractionAB = a+b;
+        System.out.println(subtractionAB);
+        return subtractionAB;
+    }
+
+    public int subtraction (int c, int d) {
+        int subtractionCD = c-d;
+        System.out.println(subtractionCD);
+        return subtractionCD;
+    }
+    public static void main (String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.addition(5,6);
+        calculator.subtraction(5,6);
+    };
+
 }
