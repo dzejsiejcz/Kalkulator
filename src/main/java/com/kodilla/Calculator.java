f1ef1a80adcb33c6683ee9d865d9b354fa9557d7
dzejsiejcz <joel.chawraj@gmail.com>
2021-11-01 20:16:30 +0100

    change returned type

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -2,16 +2,14 @@ package com.kodilla;
 
 public class Calculator {
 
-    public int addition(int a, int b) {
+    public void addition(int a, int b) {
         int subtractionAB = a+b;
         System.out.println(subtractionAB);
-        return subtractionAB;
     }
 
-    public int subtraction (int c, int d) {
+    public void subtraction (int c, int d) {
         int subtractionCD = c-d;
         System.out.println(subtractionCD + "ok");
-        return subtractionCD;
     }
     public static void main (String[] args) {
         Calculator calculator = new Calculator();
